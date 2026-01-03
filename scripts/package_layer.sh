7ff37279b6572b8b5f51b98d0e5a2d22be957832
Maxim Parkachov <mparkachov@users.noreply.github.com>
2026-01-03 15:05:13 +0100

    small adjustments

--- scripts/package_layer.sh
+++ scripts/package_layer.sh
@@ -114,6 +114,6 @@ mkdir -p "$dist_dir"
 zip_path="$dist_dir/lambda-shell-runtime-$arch.zip"
 rm -f "$zip_path"
 
-( cd "$layer_root" && zip -9 -ry "$zip_path" bootstrap bin aws-cli lib )
+( cd "$layer_root" && zip -9 -ryq "$zip_path" bootstrap bin aws-cli lib )
 
 cp "$zip_path" "$dist_dir/lambda-shell-runtime-$arch-$aws_version.zip"
